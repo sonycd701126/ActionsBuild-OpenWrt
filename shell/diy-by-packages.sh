947968a4f26f6ce3e6e24802fa8aca57052cc608
sonycd701126 <77315799+sonycd701126@users.noreply.github.com>
2022-02-26 12:19:09 +0800

    Update diy-by-packages.sh

--- shell/diy-by-packages.sh
+++ shell/diy-by-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+echo "src-git luci https://github.com/coolsnowwolf/luci">> ./feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt_Build.git" >> ./feeds.conf.default
+echo "src-git packages https://github.com/coolsnowwolf/packages" >> ./feeds.conf.default
 echo "src-git ssrp https://github.com/fw876/helloworld.git" >> ./feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> ./feeds.conf.default
 git pull
